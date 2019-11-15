216167f487a3154089674a65c14a41dd443de5ee
lileiming <lileiming@me.com>
2019-11-15 21:29:25 +0800

    Update AutoSlot.py

--- 6.Autoslot/AutoSlot.py
+++ 6.Autoslot/AutoSlot.py
@@ -14,20 +14,25 @@
 # Description		: Automatically assign hardware locations based on big data.
 #==========================================================
 
-import xlrd
 import pandas as pd
 import math
+from openpyxl import load_workbook
+#import xlrd
 #import xlwt
+import time
+import tkinter.messagebox
+
 class App():
     def __init__(self):
         self.Excelfile = 'Sample.xlsx'                                          #定义文件名
-        self.data = pd.read_excel(self.Excelfile,"Sheet0")                      #读取样本数据
+        self.data = pd.read_excel(self.Excelfile,'Sheet0')                      #读取样本数据
         self.MaxIndex = len(self.data.index)                                    #数据索引最大值 14
         self.NodeSlotList = [[] for col in range(80)]                           #所有样本数据存取 2纬 List
         self.NodeSlotCal =  [[] for col in range(80)]                           #所有样本数据比例 2纬 List
         self.Card = []                                                          #需要排布的卡件列表
+        self.outSheet = 'Out'
 
-    def myCardNum(self):
+    def my_card_num(self):
         #需要处理卡件
         total = 0
         self.rowName = list(self.data.Nodeslot.unique())                        #需要排布的卡件名目
@@ -68,21 +73,22 @@ class App():
                                     nowIndex = self.Card.index(nowCard)
                                     del self.Card[nowIndex]                     #排完的卡件，从list里删除
 
-                                    if (nowCard != "ADCV01"):                   #先拍非ADCV01卡
+                                    if (nowCard != 'ADCV01'):                   #先拍非ADCV01卡
                                         self.data.iloc[self.cardTypeNum, Q] = nowCard
                                         print(nowCard)
-                        self.Requeue()                                          #按照序号排一下
+                        self.re_queue()                                          #按照序号排一下
         #print(self.Card)
         for X in range(self.total):                                             #为拍卡的位置 排ADCV01
             if (self.data.iloc[self.cardTypeNum, X] == -1):
-                self.data.iloc[self.cardTypeNum, X] = "ADCV01"
+                self.data.iloc[self.cardTypeNum, X] = 'ADCV01'
+                print('ADCV01')
 
-    def Requeue(self):
+    def re_queue(self):
         # 按照序号排序
-        self.data = self.data.sort_values(by="NO.")
+        self.data = self.data.sort_values(by='NO.')
         self.data.index = range(self.MaxIndex)
 
-    def ReadData(self,sheetName):
+    def read_data(self,sheetName):
         #读取 单张 Sheet 卡件布置图
         data = pd.read_excel(self.Excelfile, sheetName)
         for X in range(5):
@@ -97,12 +103,16 @@ class App():
                 #print(dataR)
         #print(self.NodeSlotList)
 
-    def ReadAllData(self):
+    def read_all_data(self):
         # 读取 第一张 Sheet 后所有的 Sheet
-        for X in self.Sheetnamelist()[1:]:                                      #读取第2张 Sheet 开始的数据。
-            self.ReadData(X)
+        for X in self.sheet_name_list()[1:]:                                      #读取第2张 Sheet 开始的数据。
+            if self.outSheet not in X :
+                self.read_data(X)
+            if self.outSheet in X :
+                #print(X)
+                self.del_out_sheet(X)
 
-    def ProCalcu(self):
+    def pro_calcu(self):
         #Proportional calculation 比例计算
         Sum = 0.0
         #self.rowName = list(self.data.Nodeslot.unique())
@@ -111,7 +121,6 @@ class App():
             Sum = 0.0
             len1 = len(self.NodeSlotList[Y])
             #print(len1)
-
             for X in self.rowName[0:self.MaxIndex-2]:
                 #print(X)
                 count1 = self.NodeSlotList[Y].count(X)
@@ -124,37 +133,61 @@ class App():
             for X in range(self.total):
                 self.data.iloc[(Y), (X+2)] = self.NodeSlotCal[X][Y]
 
-    def toFile(self):
-        #输出文件
+    def del_out_sheet(self,outSheeet):
         try:
-            self.data.to_excel('out.xlsx', "Sheet0")
+            book = load_workbook(self.Excelfile)
+            #print(book[outSheeet])
+            book.remove(book[outSheeet])
+            book.save(self.Excelfile)
+            book.close()
         except PermissionError as e:
             print("输出文件未关闭")
 
+    def to_file(self):
+        #输出文件
+        try:
+            writer = pd.ExcelWriter(self.Excelfile)
+            book = load_workbook(self.Excelfile)
+            writer.book = book
+            self.data.to_excel(writer, self.outSheet)
+            #writer.save()
+            writer.close()
+        except PermissionError as e:
+            print("输出文件未关闭")
 
-    def Sheetnamelist(self):
+    def sheet_name_list(self):
         #读取Sheet名
         self.SheetnameDict = pd.read_excel(self.Excelfile,None)
         return list(self.SheetnameDict.keys())
 
     def test(self):
-        #print(self.data.NO.unique())
+        print(self.data.Nodeslot.unique())
         #print(self.data.iloc[13, 80])
         #print(self.NodeSlotCal[79][11])
         #self.data = self.data.sort_values(by='N1S1',ascending=False)
-        print(self.data)
+        #print(self.data)
         #self.data.iloc[12,1] = self.data.iloc[0,0]
-        print(self.data.iloc[12,1])
+        #print(self.data.iloc[12,1])
 
+def limited_time():
+    ticks = time.time()
+    #print(ticks)
+    limitTime = 1572414548+2592000
+    #print(limitTime)
+    localtime = time.strftime("%Y/%m/%d", time.localtime(limitTime))
+    if (ticks > limitTime):
+        tkinter.messagebox.showinfo("提示", "软件过期需要重新编译"+localtime)
+        exit()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    limited_time()
     app = App()
-    app.myCardNum()
+    app.my_card_num()
     #app.test()
-    app.ReadAllData()
-    app.ProCalcu()
+    app.read_all_data()
+    app.pro_calcu()
     app.card()
     #app.Requeue()
-    app.toFile()
+    app.to_file()
 
 
