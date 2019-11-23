9c3938f7beb6709cfad0f65a48d86b5d3562dab3
lileiming <lileiming@me.com>
2019-11-23 12:24:42 +0800

    updata 20191123

--- ModbusPVI/3.量程/PVI_ESCL.py
+++ ModbusPVI/3.量程/PVI_ESCL.py
@@ -3,70 +3,18 @@
 # python 3.7
 #==========================================================
 # Rev01
-
+# 基本实现 量程和单位的替换
+# Rev02
+# 使用自定义模块
 #==========================================================
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
-import time
-import tkinter.messagebox
-
 import os
-import xlrd
 import re
+import YokoRead   #自定义模块
 
-
-class  _FILE_NODE_:
-    #读取模块
-#    def __init__(self):
-    def read_txt(self,txt_file_name):
-        #读取txt文档
-        file_data = open(txt_file_name,'r')
-        file_detail = file_data.read()
-        return file_detail
-
-    def out_txt(self,txt_file_name,out_result):
-        out_file_name = txt_file_name
-        out_file = open(out_file_name, 'w+', encoding='GBK')  #保存为ANSI格式
-        out_file.write(out_result)
-        out_file.close()
-
-    def get_sheet(self,filename):
-        dir_case = filename
-        data = xlrd.open_workbook(dir_case)
-        sheetN = data.sheet_names()
-        return sheetN
-
-    def get_data_Tag(self,filename, sheet_name):
-        dir_case = filename
-        data = xlrd.open_workbook(dir_case)
-        table = data.sheet_by_name(sheet_name)
-        nor = table.nrows
-        nol = table.ncols
-        # print(nol)
-        dict = {}
-        # for i in range(0,nor):
-        for j in range(nol):
-            title = table.cell_value(0, j)
-            # print(title)
-            dict[title] = title
-        yield dict
-
-    def get_data(self,filename, sheet_name):
-        dir_case = filename
-        data = xlrd.open_workbook(dir_case)
-        table = data.sheet_by_name(sheet_name)
-        nor = table.nrows
-        nol = table.ncols
-        dict = {}
-        for i in range(1, nor):
-            for j in range(nol):
-                title = table.cell_value(0, j)
-                value = table.cell_value(i, j)
-                dict[title] = value
-            yield dict
-
-class PROCESS_NODE(_FILE_NODE_):
+class PROCESS_NODE(YokoRead._FILE_NODE_):
     # 处理模块
     def __init1__(self,excel_fn,sheet_n,path):
         self.excel_file_name = excel_fn
---
+++ ModbusPVI/3.量程/YokoRead.py
@@ -0,0 +1,53 @@
+import os
+import xlrd
+import re
+
+class  _FILE_NODE_:
+    #读取模块
+#    def __init__(self):
+    def read_txt(self,txt_file_name):
+        #读取txt文档
+        file_data = open(txt_file_name,'r')
+        file_detail = file_data.read()
+        return file_detail
+
+    def out_txt(self,txt_file_name,out_result):
+        out_file_name = txt_file_name
+        out_file = open(out_file_name, 'w+', encoding='GBK')  #保存为ANSI格式
+        out_file.write(out_result)
+        out_file.close()
+
+    def get_sheet(self,filename):
+        dir_case = filename
+        data = xlrd.open_workbook(dir_case)
+        sheetN = data.sheet_names()
+        return sheetN
+
+    def get_data_Tag(self,filename, sheet_name):
+        dir_case = filename
+        data = xlrd.open_workbook(dir_case)
+        table = data.sheet_by_name(sheet_name)
+        nor = table.nrows
+        nol = table.ncols
+        # print(nol)
+        dict = {}
+        # for i in range(0,nor):
+        for j in range(nol):
+            title = table.cell_value(0, j)
+            # print(title)
+            dict[title] = title
+        yield dict
+
+    def get_data(self,filename, sheet_name):
+        dir_case = filename
+        data = xlrd.open_workbook(dir_case)
+        table = data.sheet_by_name(sheet_name)
+        nor = table.nrows
+        nol = table.ncols
+        dict = {}
+        for i in range(1, nor):
+            for j in range(nol):
+                title = table.cell_value(0, j)
+                value = table.cell_value(i, j)
+                dict[title] = value
+            yield dict
